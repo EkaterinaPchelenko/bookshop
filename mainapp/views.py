e1569d72043bbe4e9abc5388108fec90549bfc46
kate_the_bee <e.pchelenko@gmail.com>
2023-10-30 20:37:50 +0300

    added products page

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,39 @@ def index(request):
 
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        "title": "bookworld",
+        "products": [
+            {"url": "#",
+             "name": "Роман1",
+             "price": "1000 руб.",
+             "description":  "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман2",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман3",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман4",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман5",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман6",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+            {"url": "#",
+             "name": "Роман7",
+             "price": "1000 руб.",
+             "description": "Какой-то роман"},
+        ]
+    }
+    return render(request, 'mainapp/products.html', context)
 
 # Create your views here.
