f593e8134534eb7a1a157f421cfd0f1906d1f594
kate_the_bee <e.pchelenko@gmail.com>
2023-10-30 19:39:12 +0300

    created base file

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import index, products
+
+import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('', mainapp.index, name='index'),
+    path('products/', mainapp.products, name='products'),
 ]
