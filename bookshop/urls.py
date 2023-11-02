a75579e933a11fd9dfcf444d622f67b6de2d43b4
kate_the_bee <e.pchelenko@gmail.com>
2023-11-02 20:10:44 +0300

    added admin

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.index, name='index'),
-    path('products/', mainapp.products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('admin/', admin.site.urls),
 ]
 
 
