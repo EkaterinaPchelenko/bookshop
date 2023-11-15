822e8145b3d49ce7f18bffc643626c68c008de26
kate_the_bee <e.pchelenko@gmail.com>
2023-11-15 20:09:55 +0300

    Worked with updating user data

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
 import users
-from users.views import profile, login, logout, register
+from users.views import login, logout, register, ProfileFormView
 
 app_name = 'users'
 urlpatterns = [
-    path('profile/', profile, name='profile'),
+    path('profile/', ProfileFormView.as_view(), name='profile'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
