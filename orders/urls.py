f25fbf4c60a45e69b794e871d1b67d9b03f5b281
kate_the_bee <e.pchelenko@gmail.com>
2023-11-22 23:07:35 +0300

    Auto stash before merge of "master" and "origin/master"

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import OrderList
+from .views import OrderList, read
 
 from mainapp.views import index
 
 app_name = 'orders'
 urlpatterns = [
     path('', OrderList.as_view(), name='list'),
+    # path('read/<int:pk>/', OrderDetail.as_view(), name='read'),
+    path('detail/', read, name='read'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
+from bookshop.mixin import BaseClassContextMixin
 from orders.models import Order
 
 
@@ -9,3 +11,15 @@ class OrderList(ListView):
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user, is_active=True)
+
+
+# class OrderDetail(DetailView, BaseClassContextMixin):
+#     model = Order
+#     title = 'BookWorld | Просмотр заказа'
+
+def read(request):
+    context = {
+        'title': 'BookWorld | Просмотр заказа',
+
+    }
+    return render(request, 'orders/order_detail.html', context)
